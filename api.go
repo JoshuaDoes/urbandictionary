1501f16615fbd181e3331e624c936e182a246143
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2018-08-02 08:41:08 -0400

    Update structs, add time parsing

--- api.go
+++ api.go
@@ -6,29 +6,36 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"time"
 )
 
+// SearchResult contains data regarding an Urban Dictionary search result
 type SearchResult struct {
-	Type    string `json:"result_type"`
-	Tags    []string
 	Results []Result `json:"list"`
-	Sounds  []string
 }
 
+// Result contains data regarding an Urban Dictionary definition
 type Result struct {
-	Author     string
-	Word       string
-	Definition string
-	Example    string
-	Permalink  string
-	Upvote     int `json:"thumbs_up"`
-	Downvote   int `json:"thumbs_down"`
+	Author       string    `json:"author"`
+	CurrentVote  string    `json:"current_vote"`
+	Date         time.Time `json:"-"`
+	Definition   string    `json:"definition"`
+	DefinitionID int       `json:"defid"`
+	Example      string    `json:"example"`
+	Permalink    string    `json:"permalink"`
+	ThumbsUp     int       `json:"thumbs_up"`
+	ThumbsDown   int       `json:"thumbs_down"`
+	Word         string    `json:"word"`
+
+	WrittenOn string `json:"written_on"`
 }
 
-const API_URL = "http://api.urbandictionary.com/v0/define?term="
+// UrbanDictionaryAPI contains the Urban Dictionary API URL for search results
+const UrbanDictionaryAPI = "http://api.urbandictionary.com/v0/define?term="
 
+// Query returns data from an Urban Dictionary search result
 func Query(searchTerm string) (*SearchResult, error) {
-	resp, err := http.Get(API_URL + url.QueryEscape(searchTerm))
+	resp, err := http.Get(UrbanDictionaryAPI + url.QueryEscape(searchTerm))
 	if err != nil {
 		return nil, err
 	}
@@ -47,6 +54,14 @@ func Query(searchTerm string) (*SearchResult, error) {
 	err = json.Unmarshal(body, res)
 	if err != nil {
 		return nil, err
+	}
+
+	for _, result := range res.Results {
+		time, err := time.Parse("2006-01-02T15:04:05.000Z", result.WrittenOn)
+		if err != nil {
+			return nil, err
+		}
+		result.Date = time
 	}
 
 	return res, nil
